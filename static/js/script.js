99ce6fb51913b5afcc02922dfc2cc626fc0edbf4
Jed Boyle <jedboyle01@gmail.com>
2020-07-02 15:03:52 +0100

    Attach a click event handler to the <li class=share> element. When clicked, toggle the corresponding <li class=share-menu> element.

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,8 @@ $(document).ready(() => {
     $('.more-btn').on('click', (event) => {
         $(event.currentTarget).siblings('.more-menu').toggle();
     })
+
+    $('.share').on('click', (event) => {
+        $(event.currentTarget).next().toggle();
+    })
 });
